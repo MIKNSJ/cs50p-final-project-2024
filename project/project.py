3b0d58c517ecabcc7d5d534c69253e85eb6a2a4b
MIKNSJ <sculetoncode@gmail.com>
2023-12-31 13:35:12 -0800

    init project

---
+++ project/project.py
@@ -0,0 +1,13 @@
+class Game:
+    def __init__(self):
+        ...
+
+    def end_win():
+        print("====================\n          You Win!          \n====================");
+
+def main():
+    test = Game();
+    Game.end_win();
+
+if __name__ == "__main__":
+    main();
